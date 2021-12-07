c251a8fbba36c69e2ce4d7db8fb2b59613f93bc4
Muddasir Vanekar <60800377+iammuddasir@users.noreply.github.com>
2021-12-07 18:58:48 +0530

    OneTap Design

--- src/Data/portfolios.js
+++ src/Data/portfolios.js
@@ -1,24 +1,33 @@
 import BBDThumbnail from '../img/portImages/BBDThumbnail.png';
 import DesignRThumbnail from '../img/portImages/DesignRThumbnail.png';
+import OneTap from '../imgs/OneTap.png';
 const portfolios = [
 	{
 		id: 1,
-		category: 'Designs',
+		category: 'Websites',
 		image: BBDThumbnail,
 		link1: 'https://www.buybydeal.com',
-		link2:
-			'https://www.behance.net/gallery/126949313/Buybydeal-20-Concept-Design.com',
+		link2: 'https://www.behance.net/gallery/126949313/Buybydeal-20-Concept-Design.com',
 		title: 'Buybydeal 2.0 Concept',
 		text: 'Created in Figma.',
 	},
 	{
 		id: 2,
-		category: 'Websites',
+		category: 'Designs',
 		image: DesignRThumbnail,
-		link1: '/comingsoon',
+		link1: 'https://comingsoon',
 		link2: 'https://www.behance.net/gallery/127537841/Design-R',
 		title: 'DesignR',
 		text: 'Created using React.',
+	},
+	{
+		id: 3,
+		category: 'Designs',
+		image: OneTap,
+		link1: 'https://comingsoon',
+		link2: 'https://www.behance.net/gallery/132149357/OneTap-Smart-Lock-App',
+		title: 'Buybydeal 2.0 Concept',
+		text: 'Created in Figma.',
 	},
 ];
 
